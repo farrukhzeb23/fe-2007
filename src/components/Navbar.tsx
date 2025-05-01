0fc73d7a342bede96c0fe49550880c318ec03e56
Farrukh zeb <farrukh.zeb@emumba.com>
2025-05-01 12:59:08 +0500

    feat: add magnify icon and implement search bar in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,16 @@
+import MagnifyIcon from '../assets/icons/magnify.svg';
+
 function Navbar() {
   return (
     <nav className="navbar">
       <img src="/images/logo.svg" alt="logo" className="logo" />
+      <div className="navbar-actions">
+        <div className="search-bar">
+          <img src={MagnifyIcon} alt="magnify" className="icon" />
+          <input type="text" name="search" id="search" placeholder="Search Gists..." />
+        </div>
+        <button>Login</button>
+      </div>
     </nav>
   );
 }
